95801e42911b5f8b5bbf391dedefe5907b9772eb
Barret Anspach <barret.anspach@gmail.com>
2024-09-19 13:38:02 -0700

    removed explicitscore

--- pages/explicitScore.js
+++
@@ -1,7 +0,0 @@
-import ExplicitScoreExample from '../components/Score/ExplicitScore.example';
-
-const score = require('../fixtures/2024-08-29--test.json');
-
-export default function Minim() {
-  return <ExplicitScoreExample score={score} />;
-}

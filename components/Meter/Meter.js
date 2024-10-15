a722e3a4a1b112a62070afa8bf9620442b9c6b80
Barret Anspach <barret.anspach@gmail.com>
2024-10-14 21:28:03 -0700

    meter glyphs added

--- components/Meter/Meter.js
+++ components/Meter/Meter.js
@@ -1,6 +1,14 @@
+import { useMemo } from "react";
+
 import { getPitches } from "../../utils/getPitches";
 import Item from "../Item";
 import StaffDisplayItem from "../StaffDisplayItem";
+import { getDiatonicTransposition } from "../../utils/methods";
+
+const meterGlyphMap = {
+  key: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  value: ["", "", "", "", "", "", "", "", "", ""],
+};
 
 export default function Meter({
   type = "regular",
@@ -10,18 +18,43 @@ export default function Meter({
   unit,
   start = "m-tim",
 }) {
-  const style = getPitches(clef);
+  const {
+    rangeClef: { midline },
+  } = getPitches(clef);
+  const meter = useMemo(
+    () => ({
+      numerator: {
+        pitch: `${id}${getDiatonicTransposition(midline, 2)}`,
+        glyphs: Array.from({ length: count.toString().length }, (_, i) =>
+          count.toString().slice(i, i + 1),
+        ).map(
+          (digit) =>
+            meterGlyphMap.value[meterGlyphMap.key.find((k) => k === digit)],
+        ),
+      },
+      denominator: {
+        pitch: `${id}${getDiatonicTransposition(midline, -2)}`,
+        glyphs: Array.from({ length: unit.toString().length }, (_, i) =>
+          unit.toString().slice(i, i + 1),
+        ).map(
+          (digit) =>
+            meterGlyphMap.value[meterGlyphMap.key.find((k) => k === digit)],
+        ),
+      },
+    }),
+    [count, id, midline, unit],
+  );
 
   switch (type) {
     case "regular":
     default: {
       return (
         <StaffDisplayItem type="tim" start={start}>
-          <Item text pitch={`${id}${style.rangeClef.midline}`}>
-            {count}
+          <Item pitch={meter.numerator.pitch}>
+            {meter.numerator.glyphs.map((glyph) => glyph)}
           </Item>
-          <Item text pitch={`${id}${style.staffBounds.lower.id}`}>
-            {unit}
+          <Item pitch={meter.denominator.pitch}>
+            {meter.denominator.glyphs.map((glyph) => glyph)}
           </Item>
         </StaffDisplayItem>
       );
